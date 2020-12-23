57fd2423ea4792c4cc429637269e37bb9781afd7
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-12-23 22:11:19 +0100

    Add Entities

---
+++ src/WebShop.Domain/Entities/EntitiesObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Domain.Entities
+{
+    public class EntitiesObject
+    {
+        int Id { get; set; }
+    }
+}
---
+++ src/WebShop.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Domain.Entities
+{
+    public class User : EntitiesObject
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
