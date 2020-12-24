3bd18549ef9e1a16c88fca862216c7817880395d
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-12-24 10:25:55 +0100

    Add DbContext

---
+++ src/WebShop.Infrastructure/WebShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShop.Domain.Entities;
+
+namespace WebShop.Infrastructure
+{
+    public class WebShopDbContext : DbContext
+    {
+        public WebShopDbContext(DbContextOptions<WebShopDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/WebShop.Infrastructure/WebShopDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WebShop.Infrastructure
+{
+    public class WebShopDbContextFactory
+    {
+        private readonly string _connectionString;
+        public WebShopDbContextFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public WebShopDbContext CreateDbContext()
+        {
+            DbContextOptionsBuilder<WebShopDbContext> options = new DbContextOptionsBuilder<WebShopDbContext>();
+
+            options.UseSqlServer(_connectionString);
+
+            return new WebShopDbContext(options.Options);
+        }
+    }
+}
