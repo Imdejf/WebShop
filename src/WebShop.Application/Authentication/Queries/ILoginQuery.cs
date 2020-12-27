4656cace65d63c8ac756a5031487b5181937393c
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-12-27 22:01:50 +0100

    Add query LoginQuery

---
+++ src/WebShop.Application/Authentication/Queries/ILoginQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WebShop.Domain.Entities;
+
+namespace WebShop.Application.Authentication.Queries
+{
+    public interface ILoginQuery
+    {
+        Task<Account> Login(string username, string password);
+    }
+}
---
+++ src/WebShop.Application/Authentication/Queries/LoginQuery.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Threading.Tasks;
+using WebShop.Domain.Entities;
+using WebShop.Domain.Interfaces;
+
+namespace WebShop.Application.Authentication.Queries
+{
+    public class LoginQuery : ILoginQuery
+    {
+        private readonly IAccountService _accountService;
+        private readonly IPasswordHasher _passwordHasher;
+        public LoginQuery(IAccountService accountService, IPasswordHasher passwordHasher)
+        {
+            _accountService = accountService;
+            _passwordHasher = passwordHasher;
+        }
+        public async Task<Account> Login(string username, string password)
+        {
+            Account storedAccount = await _accountService.GetByUserName(username);
+            if(storedAccount == null)
+            {
+                throw new Exception();
+            }
+            PasswordVerificationResult verificationResult = _passwordHasher.VerifyHashedPassword(storedAccount.AccountHolder.PasswordHash, password);
+            if(verificationResult != PasswordVerificationResult.Success)
+            {
+                throw new Exception();
+            }
+            return storedAccount;
+        }
+
+    }
+}
