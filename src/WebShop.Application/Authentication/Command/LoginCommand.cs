7f8b2cc8d828fe029c613098cd27fd0ff02cdae7
Dawid Jablonski <jablonskidawid0202@gmail.com>
2020-12-30 21:48:27 +0100

    Add LoginCommand with event handler

---
+++ src/WebShop.Application/Authentication/Command/LoginCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WebShop.Application.Authentication.Command
+{
+    public class LoginCommand : IRequest<int>
+    {
+        public int Result { get; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/WebShop.Application/Authentication/Command/LoginCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using WebShop.Application.Authentication.Queries;
+using WebShop.Application.Models.Login;
+using WebShop.Domain.Entities;
+using WebShop.Domain.Services;
+using WebShop.Infrastructure;
+
+namespace WebShop.Application.Authentication.Command
+{
+    public class LoginCommandHandler : IRequestHandler<LoginCommand, int>
+    {
+        private readonly IAuthenticationService _authenticationService;
+        public LoginCommandHandler(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+        public async Task<int> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _authenticationService.Login(request.Username, request.Password);
+            return result.Id;
+        }
+    }
+}
