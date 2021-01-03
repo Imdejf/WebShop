5d19a2c6806c09c4aeee6e3ce37c6d015d2942f0
Dawid Jablonski <jablonskidawid0202@gmail.com>
2021-01-03 23:47:01 +0100

    Add SelectedRoleRequirement

---
+++ src/WebShop.Application/Common/Requirement/SelectedRoleRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace WebShop.Application.Common.Requirement
+{
+    public class SelectedRoleRequirement : IAuthorizationRequirement
+    {
+        public string Role { get; set; }
+        public SelectedRoleRequirement(string role)
+        {
+            Role = role;
+        }
+    }
+}
