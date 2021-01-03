506afaa3cc4fc7e36f6c728dadd5ce0934ab2b84
Dawid Jablonski <jablonskidawid0202@gmail.com>
2021-01-03 23:45:15 +0100

    Add SelectedRoleHandler

---
+++ src/WebShop.Application/Common/Handlers/SelectedRoleHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Application.Common.Requirement;
+
+namespace WebShop.Application.Common.Handlers
+{
+    public class SelectedRoleHandler : AuthorizationHandler<SelectedRoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, SelectedRoleRequirement selectedRole)
+        {
+            if (!context.User.HasClaim(c => c.Type == ClaimTypes.Role))
+            {
+                return Task.CompletedTask;
+            }
+
+            if (selectedRole.Role.Length > 0)
+            {
+                context.Succeed(selectedRole);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
